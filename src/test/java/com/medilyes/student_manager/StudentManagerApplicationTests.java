9ff220887a9765424ab1621cc4f84f286f6b2afc
medilyes <khelilmohammedilyes3@gmail.com>
2025-01-22 09:03:46 +0100

    first commit

---
+++ src/test/java/com/medilyes/student_manager/StudentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilyes.student_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
